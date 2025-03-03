b3788c61caea3104cbf0da996b1dd0fa957e6d31
johnmaxrin <johnmaxrin@gmail.com>
2025-03-03 16:51:20 +0530

    Add vertex to edge transformation condition check

--- starplatDialect/transforms/vertexToEdge.h
+++ starplatDialect/transforms/vertexToEdge.h
@@ -16,15 +16,65 @@ namespace mlir
         {
             using VertexToEdgeBase::VertexToEdgeBase;
 
+            /*
+                TODOs
+                1. Add checks for whether we are traversing all the nodes in a gprah g.
+            */
+
             void runOnOperation() override
             {
                 auto mod = getOperation();
+                mod->walk<mlir::WalkOrder::PreOrder>([&](mlir::Operation *op) {
+                   if(llvm::isa<mlir::starplat::ForAllOp>(op))
+                   {
+                        auto attr = op->getAttr("loopattributes");
+                        if(attr)
+                        {
+                            if(auto arrAttr = attr.dyn_cast<mlir::ArrayAttr>())
+                            {
+                                for(auto attrElm : arrAttr)
+                                {
+                                    if(auto strAttr = attrElm.dyn_cast<mlir::StringAttr>())
+                                    {
+                                        if(strAttr.getValue() == "nodes")
+                                        {
+                                            llvm::outs() << "We have a forall loop which iterates over all the nodes.\n";
+                                            op->walk<mlir::WalkOrder::PreOrder>([&](mlir::starplat::ForAllOp op)
+                                            {
+                                                auto nbrAttr = op->getAttr("loopattributes");
+                                                if(nbrAttr)
+                                                {
+                                                    if(auto nbrArrAttr = nbrAttr.dyn_cast<mlir::ArrayAttr>())
+                                                    {
+                                                        for(auto nbrAttrElm : nbrArrAttr)
 
-                mod->walk([&](mlir::Operation *op) {
+                                                        if(auto nbrStrAttr = nbrAttrElm.dyn_cast<mlir::StringAttr>())
+                                                        {
+                                                            if(nbrStrAttr.getValue() == "neighbours")
+                                                            {
+                                                                llvm::outs() << "We have another forall loop which iterates over all neighbours of a node.\n";
+                                                                llvm::outs() << "Vertex to edge transformation is feasible.\n";
+                                                            }
+                                                        }
+                                                    }
+                                                }
+
+                                            });
+
+
+                                        }
+                                    }
+                                }
+
+                            }
+                        }
+
+                        
+                   }
 
                 });
 
-                llvm::outs() << "Vertex to Edge Transform\n";
+                llvm::outs() << "Finished Vertex to Edge Transform\n";
             }
         };
 
