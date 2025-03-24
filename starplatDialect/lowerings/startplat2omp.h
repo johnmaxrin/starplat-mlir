8cf54a74ae66936a7a3fc28b757714d5db2409d5
johnmaxrin <johnmaxrin@gmail.com>
2025-03-24 11:27:11 +0530

    Add Graph and Node Struct in LLVM and Rewrite Args to LLVMIR

--- starplatDialect/lowerings/startplat2omp.h
+++ starplatDialect/lowerings/startplat2omp.h
@@ -12,6 +12,9 @@
 //     return;
 // }
 
+mlir::LLVM::LLVMStructType createGraphStruct(mlir::IRRewriter *rewriter, mlir::MLIRContext *context);
+mlir::LLVM::LLVMStructType createNodeStruct(mlir::IRRewriter *rewriter, mlir::MLIRContext *context);
+
 namespace mlir
 {
     namespace starplat
@@ -28,34 +31,88 @@ namespace mlir
                 auto mod = getOperation();
                 mod->walk<mlir::WalkOrder::PreOrder>([&](mlir::starplat::FuncOp funcOp)
                                                      {
-                                                         funcOp->getContext()->getOrLoadDialect<mlir::LLVM::LLVMDialect>();
-                                                         mlir::IRRewriter rewriter(funcOp->getContext());
-                                                         mlir::OpBuilder::InsertionGuard guard(rewriter);
+                    funcOp->getContext()->getOrLoadDialect<mlir::LLVM::LLVMDialect>();
+                    mlir::IRRewriter rewriter(funcOp->getContext());
+                    mlir::OpBuilder::InsertionGuard guard(rewriter);
 
-                                                         auto &entryBlock = funcOp.getBody().getBlocks().front();
-                                                         rewriter.setInsertionPointToStart(&entryBlock);
+                    auto &entryBlock = funcOp.getBody().getBlocks().front();
+                    rewriter.setInsertionPointToStart(&entryBlock);
+                    // CSR For Vertex based.
+                    // COO for Edge based.
 
-                                                         // CSR For Vertex based.
-                                                         // COO for Edge based.
-
-                                                         // Check arg ops and declare structs. Eg Graph, Nodes etc
-                                                         funcOp->walk<mlir::WalkOrder::PreOrder>([&](mlir::Operation *arg)
-                                                                                                 {
+                    // Check arg ops and declare structs. Eg Graph, Nodes etc
+                    llvm::SmallVector<mlir::Operation *, 4> toErase;
+                    funcOp->walk<mlir::WalkOrder::PreOrder>([&](mlir::starplat::ArgOp arg)
+                    {
                             if (auto typeAttr = arg->getAttrOfType<mlir::TypeAttr>("type")) {
                                 mlir::Type argType = typeAttr.getValue();
                     
                                 // Check if argType is the specific Starplat type
                                 if (argType.isa<mlir::starplat::GraphType>()) {
-                                    llvm::outs() << "Graph Type!\n";
-                                    auto structType = LLVM::LLVMStructType::getIdentified(funcOp->getContext(), "Graph");
-                                    structType.setBody({rewriter.getI32Type()},false);
 
-                                    auto alloc = rewriter.create<LLVM::AllocaOp>(rewriter.getUnknownLoc(), LLVM::LLVMPointerType::get(funcOp.getContext()), structType, rewriter.create<LLVM::ConstantOp>(rewriter.getUnknownLoc(), rewriter.getI64Type(), rewriter.getI64IntegerAttr(1)));
+                                    auto graphStruct = createGraphStruct(&rewriter, funcOp->getContext());
+                                    auto alloc = rewriter.create<LLVM::AllocaOp>(rewriter.getUnknownLoc(), LLVM::LLVMPointerType::get(funcOp->getContext()), graphStruct, rewriter.create<LLVM::ConstantOp>(rewriter.getUnknownLoc(), rewriter.getI64Type(), rewriter.getI64IntegerAttr(1)));
+
+                                    if(arg->hasAttr("sym_name")){
+                                        auto symNameAttr = arg->getAttrOfType<mlir::StringAttr>("sym_name");
+                                        if(symNameAttr){
+                                            alloc->setAttr("sym_name", rewriter.getStringAttr(symNameAttr.getValue()));
+                                        }
+                                    }
+
+                                    if (arg->use_empty()) {
+                                        toErase.push_back(arg);
+                                    } else {
+                                        arg->replaceAllUsesWith(alloc);
+                                        toErase.push_back(arg);
+                                    }
+
+                                }
+
+                                if(argType.isa<mlir::starplat::NodeType>()) {
+                                    auto nodeStruct = createNodeStruct(&rewriter, funcOp->getContext());
+                                    auto alloc = rewriter.create<LLVM::AllocaOp>(rewriter.getUnknownLoc(), LLVM::LLVMPointerType::get(funcOp->getContext()), nodeStruct, rewriter.create<LLVM::ConstantOp>(rewriter.getUnknownLoc(), rewriter.getI64Type(), rewriter.getI64IntegerAttr(1)));
+                                    
+                                    if (arg->hasAttr("sym_name")) {
+                                        auto symNameAttr = arg->getAttrOfType<mlir::StringAttr>("sym_name");
+                                        if (symNameAttr) {
+                                            alloc->setAttr("sym_name", rewriter.getStringAttr(symNameAttr.getValue()));
+                                        }
+                                    }
+
+                                    if (arg->use_empty()) {
+                                        toErase.push_back(arg);
+                                    } else {
+                                        arg->replaceAllUsesWith(alloc);
+                                        toErase.push_back(arg);
+                                    }
                                     
                                 }
-                            } });
-                                                     });
+                            }}); 
+                        
+                            for (mlir::Operation *op : toErase) {
+                                op->erase();
+                            }
+                        
+                        
+                });
             }
         };
     }
+}
+
+mlir::LLVM::LLVMStructType createGraphStruct(mlir::IRRewriter *rewriter, mlir::MLIRContext *context)
+{
+    auto structType = LLVM::LLVMStructType::getIdentified(context, "Graph");
+    structType.setBody({rewriter->getI32Type()}, false);
+
+    return structType;
+}
+
+mlir::LLVM::LLVMStructType createNodeStruct(mlir::IRRewriter *rewriter, mlir::MLIRContext *context)
+{
+    auto structType = LLVM::LLVMStructType::getIdentified(context, "Node");
+    structType.setBody({rewriter->getI32Type()}, false);
+
+    return structType;
 }
