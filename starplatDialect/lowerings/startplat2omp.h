be6061dbad86c174dcdd0244b56383f05160227f
johnmaxrin <johnmaxrin@gmail.com>
2025-04-12 23:07:47 +0530

    Add Conversion Pattern Interfaces

--- starplatDialect/lowerings/startplat2omp.h
+++ starplatDialect/lowerings/startplat2omp.h
@@ -6,6 +6,8 @@
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/Dialect/SCF/IR/SCF.h"
 
+#include "mlir/Transforms/DialectConversion.h"  // from @llvm-project
+
 // mlir::Operation *generateGraphStruct(mlir::MLIRContext *context)
 // {
 //     auto structType = LLVM::LLVMStructType::getIdentified(context, "Graph");
@@ -15,9 +17,21 @@
 
 mlir::LLVM::LLVMStructType createGraphStruct(mlir::IRRewriter *rewriter, mlir::MLIRContext *context);
 mlir::LLVM::LLVMStructType createNodeStruct(mlir::IRRewriter *rewriter, mlir::MLIRContext *context);
-void lowerFunctionOp(mlir::Operation *op, mlir::IRRewriter *rewriter);
-void lowerFixedPointUntilOp(mlir::Operation *op, mlir::IRRewriter *rewriter);
-void lowerForAllOp(mlir::Operation *op, mlir::IRRewriter *rewriter);
+
+struct ConvertAdd : public OpConversionPattern<mlir::starplat::FuncOp>
+{
+    using OpConversionPattern::OpConversionPattern;
+
+    LogicalResult matchAndRewrite(
+        mlir::starplat::FuncOp op, OpAdaptor adaptor,
+        ConversionPatternRewriter &rewriter) const override
+    {
+
+        llvm::errs() << "Hi Guys!!!!!\n\n\n";
+
+        return success();
+    }
+};
 
 namespace mlir
 {
@@ -32,20 +46,20 @@ namespace mlir
 
             void runOnOperation() override
             {
-                auto mod = getOperation();
-                mlir::Region &region = mod->getRegion(0);
-                mlir::Block &block = region.front();
+                mlir::MLIRContext *context = &getContext();
+                auto *module = getOperation();
 
-                mlir::IRRewriter rewriter(mod->getContext());
-                mlir::OpBuilder::InsertionGuard guard(rewriter);
+                ConversionTarget target(*context);
+                //target.addLegalDialect<arith::ArithDialect>();
+                target.addIllegalOp<mlir::starplat::AddOp>();
 
-                auto graphType = createGraphStruct(&rewriter, rewriter.getContext());
-                auto nodeType = createNodeStruct(&rewriter, rewriter.getContext());
+                RewritePatternSet patterns(context);
+                // PolyToStandardTypeConverter typeConverter(context);
+                patterns.add<ConvertAdd>(context);
 
-                for (mlir::Operation &op : block.getOperations())
+                if (failed(applyPartialConversion(module, target, std::move(patterns))))
                 {
-                    if (llvm::isa<mlir::starplat::FuncOp>(op))
-                        lowerFunctionOp(&op, &rewriter);
+                    signalPassFailure();
                 }
 
                 // mod->dump();
@@ -53,59 +67,6 @@ namespace mlir
         };
     }
 }
-
-void lowerFunctionOp(mlir::Operation *op, mlir::IRRewriter *rewriter)
-{
-    mlir::Region &region = op->getRegion(0);
-    mlir::Block &block = region.front();
-    
-    for (mlir::Operation &op : block.getOperations())
-    {
-        if (llvm::isa<mlir::starplat::FixedPointUntilOp>(op))
-            lowerFixedPointUntilOp(&op, rewriter);
-    }
-}
-
-void lowerFixedPointUntilOp(mlir::Operation *op, mlir::IRRewriter *rewriter)
-{
-    mlir::Region &region = op->getRegion(0);
-    mlir::Block &block = region.front();
-    
-    for (mlir::Operation &op : block.getOperations())
-    {
-        if (llvm::isa<mlir::starplat::ForAllOp>(op))
-            lowerForAllOp(&op, rewriter);
-    }
-}
-
-void lowerForAllOp(mlir::Operation *op, mlir::IRRewriter *rewriter)
-{
-    mlir::Region &region = op->getRegion(0);
-    mlir::Block &block = region.front();
-
-    rewriter->setInsertionPointToStart(&block);
-    
-    for (mlir::Operation &loopOp : block.getOps())
-    {
-        
-
-       if(llvm::isa<mlir::starplat::ForAllOp>(op))
-        {
-
-            auto i32Type = rewriter->getI32Type();
-            auto i32O = rewriter->getI32IntegerAttr(0);
-            auto i321 = rewriter->getI32IntegerAttr(1);
-            auto i3210 = rewriter->getI32IntegerAttr(10);
-            auto const0 = rewriter->create<mlir::LLVM::ConstantOp>(rewriter->getUnknownLoc(), i32Type, i32O);
-            auto const1 = rewriter->create<mlir::LLVM::ConstantOp>(rewriter->getUnknownLoc(), i32Type, i321);
-            auto const10 = rewriter->create<mlir::LLVM::ConstantOp>(rewriter->getUnknownLoc(), i32Type, i3210);
-            auto sample = rewriter->create<mlir::scf::ForOp>(rewriter->getUnknownLoc(), const0->getResult(0), const10->getResult(0), const1->getResult(0));
-
-            loopOp.replaceAllUsesWith(sample);
-        }
-    }
-}
-
 
 mlir::LLVM::LLVMStructType createGraphStruct(mlir::IRRewriter *rewriter, mlir::MLIRContext *context)
 {
