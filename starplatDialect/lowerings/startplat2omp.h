43ce3ed2431ece68771dc592525d9a9548119340
johnmaxrin <johnmaxrin@gmail.com>
2025-04-13 23:03:25 +0530

    Minor Updates

--- starplatDialect/lowerings/startplat2omp.h
+++ starplatDialect/lowerings/startplat2omp.h
@@ -6,7 +6,7 @@
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/Dialect/SCF/IR/SCF.h"
 
-#include "mlir/Transforms/DialectConversion.h"  // from @llvm-project
+#include "mlir/Transforms/DialectConversion.h" // from @llvm-project
 
 // mlir::Operation *generateGraphStruct(mlir::MLIRContext *context)
 // {
@@ -18,7 +18,23 @@
 mlir::LLVM::LLVMStructType createGraphStruct(mlir::IRRewriter *rewriter, mlir::MLIRContext *context);
 mlir::LLVM::LLVMStructType createNodeStruct(mlir::IRRewriter *rewriter, mlir::MLIRContext *context);
 
-struct ConvertAdd : public OpConversionPattern<mlir::starplat::FuncOp>
+class StarplatToLLVMTypeConverter : public TypeConverter
+{
+public:
+    StarplatToLLVMTypeConverter(MLIRContext *ctx)
+    {
+        addConversion([](Type type)
+                      { return type; });
+
+        addConversion([ctx](mlir::starplat::GraphType graph)
+                      { return mlir::IntegerType::get(ctx, 32, mlir::IntegerType::SignednessSemantics::Signless); });
+
+        addConversion([ctx](mlir::starplat::NodeType node)
+                      { return mlir::IntegerType::get(ctx, 32, mlir::IntegerType::SignednessSemantics::Signless); });
+    }
+};
+
+struct ConvertFunc : public OpConversionPattern<mlir::starplat::FuncOp>
 {
     using OpConversionPattern::OpConversionPattern;
 
@@ -26,9 +42,52 @@ struct ConvertAdd : public OpConversionPattern<mlir::starplat::FuncOp>
         mlir::starplat::FuncOp op, OpAdaptor adaptor,
         ConversionPatternRewriter &rewriter) const override
     {
+        llvm::outs() << "Func Matched\n";
+        auto loc = op.getLoc();
+        auto returnType = rewriter.getI32Type();
+        auto i32Type = rewriter.getI32Type();
 
-        llvm::errs() << "Hi Guys!!!!!\n\n\n";
+        // Function signature: (i32, i32) -> i32
+        auto oldFuncType = op.getFuncType();
+        SmallVector<Type> paramTypes = {oldFuncType.getInput(0), oldFuncType.getInput(1), oldFuncType.getInput(2), oldFuncType.getInput(3)};
+ 
+        
+        auto funcType = LLVM::LLVMFunctionType::get(returnType, paramTypes, /*isVarArg=*/false);
+        auto funcName = op.getSymName();
 
+        // Create the LLVM function
+        auto funcOp = rewriter.create<LLVM::LLVMFuncOp>(loc, funcName, funcType);
+
+        // Create an entry block with the right number of arguments
+        // Block *entryBlock = rewriter.createBlock(&funcOp.getBody(), {}, paramTypes, {loc, loc});
+
+        rewriter.inlineRegionBefore(op.getRegion(), funcOp.getBody(), funcOp.end());
+        rewriter.eraseOp(op);
+
+        // Replace original starplat.func
+
+        funcOp->dump();
+
+        return success();
+    }
+};
+
+struct ConvertAdd : public OpConversionPattern<mlir::starplat::AddOp>
+{
+    using OpConversionPattern::OpConversionPattern;
+
+    LogicalResult matchAndRewrite(
+        mlir::starplat::AddOp op, OpAdaptor adaptor,
+        ConversionPatternRewriter &rewriter) const override
+    {
+
+        llvm::outs() << "Add Matched\n";
+
+        auto addOp = rewriter.create<LLVM::AddOp>(op.getLoc(), rewriter.getI32Type(), op->getOperand(0), op->getOperand(1));
+
+        addOp.dump();
+
+        rewriter.replaceOp(op.getOperation(), addOp);
         return success();
     }
 };
@@ -50,19 +109,21 @@ namespace mlir
                 auto *module = getOperation();
 
                 ConversionTarget target(*context);
-                //target.addLegalDialect<arith::ArithDialect>();
+
+                target.addLegalOp<mlir::LLVM::LLVMFuncOp>();
+                target.addLegalOp<mlir::LLVM::AddOp>();
+
+                target.addIllegalOp<mlir::starplat::FuncOp>();
                 target.addIllegalOp<mlir::starplat::AddOp>();
 
                 RewritePatternSet patterns(context);
-                // PolyToStandardTypeConverter typeConverter(context);
-                patterns.add<ConvertAdd>(context);
+                StarplatToLLVMTypeConverter typeConverter(context);
+                patterns.add<ConvertAdd, ConvertFunc>(typeConverter, context);
 
                 if (failed(applyPartialConversion(module, target, std::move(patterns))))
                 {
                     signalPassFailure();
                 }
-
-                // mod->dump();
             }
         };
     }
